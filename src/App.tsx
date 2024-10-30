82d140393bc06e51d324b2fb7e5484224ea1a7a2
Choiji92 <jeahoon100@naver.com>
2024-10-30 12:18:04 +0900

    [FEAT] 로그인 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from '@pages/Main'
 import Signup from '@pages/Signup'
 import Login from '@pages/Login'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Main />
+            </PrivateRoute>
+          }
+        />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
